47600a31ea593543ad3520ed6a691459982d762d
Jeffrey David Johnson <jefdaj@protonmail.ch>
2019-11-19 17:31:01 +0000

    try sophia, and fail so far

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//use sophia::dataset::inmem::HashDataset;
+
+// pub struct AnonDB {
+	// db: HashDataset,
+// }
+
+// impl AnonDB {
+	// pub fn new() -> AnonDB {
+		// // AnonDB { db: SetDataset::new() }
+	// }
+	// pub fn inserter(&mut self) { self.db.inserter }
+	// pub fn remover(&mut self) { self.db.remover }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,33 @@
-extern crate rio_api;
-extern crate rio_turtle;
-extern crate rio_xml;
-extern crate sophia;
+// extern crate anondb;
+use sophia::graph::inmem::FastGraph;
+use sophia::parser::nt;
+use sophia::triple::stream::*;
+
+use sophia::dataset::inmem::small::FastDataset;
+
+static NT_DOC: &str = r#"
+  <http://champin.net/#pa> <http://schema.org/name> "Pierre-Antoine Champin".
+"#;
 
 fn main() {
-  println!("Hello world!");
-}
+  // learning sophia
+  let mut g = FastGraph::new();
+  let inserted = nt::parse_str(NT_DOC).in_graph(&mut g);
+  println!("inserted: {}", inserted.unwrap());
 
-// TODO overall data structure?
-// TODO wait, do the signatures not really need to be stored after verification once?
-// TODO create a graph
-// TODO read gpg turtle file
-// TODO check signature first
-// TODO parse the ttl
-// TODO add it to the graph
+  let mut g2 = FastDataset::new();
+  let inserted2 = nt::parse_str(NT_DOC).in_graph(&mut g2);
+  println!("inserted2: {}", inserted2.unwrap());
+  // println!("g: {}", g.);
+
+  // let mut db = AnonDB::new();
+
+  // TODO wait, do the signatures not really need to be stored after verification once?
+  // TODO create a graph
+  // TODO read gpg turtle file
+  // TODO check signature first
+  // TODO parse the ttl
+  // TODO add it to the graph
+
+  // println!("Hello world!");
+}
