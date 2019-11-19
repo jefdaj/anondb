0a5bd8cefd21e11a640d1f5679f031ac7927e8ca
Jeffrey David Johnson <jefdaj@protonmail.ch>
2019-11-19 18:38:40 +0000

    parse example02.trig

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,8 @@
 
 use rio_turtle::{TriGParser, TurtleError};
 use rio_api::parser::QuadsParser;
-use rio_api::model::NamedNode;
+// use rio_api::model::NamedNode;
+use std::fs;
 
 fn main() {
   // let file = b"<http://example.com/foo> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> <http://example.com/> .
@@ -27,21 +28,31 @@ fn main() {
   //   <http://example.com/bar> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
   //   <http://example.com/bar> <http://schema.org/name> \"Bar\" .";
 
-  let file2 = b"@prefix schema: <http://schema.org/> .
-  <http://example/> {
-      <http://example.com/foo> a schema:Person ; schema:name  \"Foo\" .
-      <http://example.com/bar> a schema:Person ; schema:name  \"Bar\" .
-  }";
+  // let file2 = b"@prefix schema: <http://schema.org/> .
+  // <http://example/> {
+  //     <http://example.com/foo> a schema:Person ; schema:name  \"Foo\" .
+  //     <http://example.com/bar> a schema:Person ; schema:name  \"Bar\" .
+  // }";
 
-  let rdf_type = NamedNode { iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" };
-  let schema_person = NamedNode { iri: "http://schema.org/Person" };
+  // let rdf_type = NamedNode { iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" };
+  // let schema_person = NamedNode { iri: "http://schema.org/Person" };
   let mut count = 0;
-  TriGParser::new(file2.as_ref(), "").unwrap().parse_all(&mut |t| {
-      println!("parsed quad: {}", t);
-      if t.predicate == rdf_type && t.object == schema_person.into() {
-          count += 1;
-      }
+
+  let filename = "example02.trig";
+  println!("parsing file: {}", filename);
+  let contents = fs::read_to_string(filename)
+      .expect("Something went wrong reading the file");
+
+  TriGParser::new(contents.as_ref(), "").unwrap().parse_all(&mut |t| {
+      println!("quad: {}", t);
+      // println!("\tsubject: {}", t.subject);
+      // println!("\tpredicate: {}", t.predicate);
+      // println!("\tobject: {}", t.object);
+      // if t.predicate == rdf_type && t.object == schema_person.into() {
+      count += 1;
+      // }
       Ok(()) as Result<(), TurtleError>
   }).unwrap();
-  assert_eq!(2, count)
+  println!("quads read: {}", count);
+  // assert_eq!(2, count)
 }
