c4bca1008f6a9c21edfc5a07eb219d8fac71bf97
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-01-29 22:56:55 +0000

    cli parses multiple rdf files

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// TODO parse any rdf format, either automatically or with a cli format flag
-// 24 days of rust: http://zsiciarz.github.io/24daysofrust/index.html
-
 // extern crate rio_api;
 // extern crate rio_turtle;
 // extern crate rio_xml;
@@ -34,46 +31,29 @@ Options:
 ";
 
 fn main() {
-  // Parse argv and exit the program with an error message if it fails.
   let args = Docopt::new(USAGE)
                      .and_then(|d| d.parse())
                      .unwrap_or_else(|e| e.exit());
   let rdfs = args.get_vec("<rdf>");
   println!("rdf files to parse: {:?}", rdfs);
 
-  // let file = b"<http://example.com/foo> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> <http://example.com/> .
-  //   <http://example.com/foo> <http://schema.org/name> \"Foo\" <http://example.com/> .
-  //   <http://example.com/bar> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://schema.org/Person> .
-  //   <http://example.com/bar> <http://schema.org/name> \"Bar\" .";
+  // let mut count = 0;
 
-  // let file2 = b"@prefix schema: <http://schema.org/> .
-  // <http://example/> {
-  //     <http://example.com/foo> a schema:Person ; schema:name  \"Foo\" .
-  //     <http://example.com/bar> a schema:Person ; schema:name  \"Bar\" .
-  // }";
+  for rdf in rdfs {
+    println!("parsing {:?}...", rdf);
+    let contents = fs::read_to_string(rdf)
+        .expect("parse error :(");
 
-  // let rdf_type = NamedNode { iri: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" };
-  // let schema_person = NamedNode { iri: "http://schema.org/Person" };
-  let mut count = 0;
-
-  // example01.ttl works too, if you use TurtleParser
-  let filename = "example02.trig";
-  println!("parsing {}...", filename);
-  let contents = fs::read_to_string(filename)
-      .expect("Something went wrong reading the file");
-
-  TriGParser::new(contents.as_ref(), "").unwrap().parse_all(&mut |t| {
-  // TurtleParser::new(contents.as_ref(), "file:example01.ttl").unwrap().parse_all(&mut |t| {
-      println!("statement: {}", t);
-      // println!("\tgraph: {}", t.graph);
-      // println!("\tsubject: {}", t.subject);
-      // println!("\tpredicate: {}", t.predicate);
-      // println!("\tobject: {}", t.object);
-      // if t.predicate == rdf_type && t.object == schema_person.into() {
-      count += 1;
-      // }
-      Ok(()) as Result<(), TurtleError>
-  }).unwrap();
-  println!("parsed {} statements", count);
-  // assert_eq!(2, count)
+    TriGParser::new(contents.as_ref(), "").unwrap().parse_all(&mut |t| {
+        println!("statement: {}", t);
+        println!("\tsubject: {}", t.subject);
+        println!("\tpredicate: {}", t.predicate);
+        println!("\tobject: {}", t.object);
+        println!("\tgraph: {:?}", t.graph_name);
+        // if t.predicate == rdf_type && t.object == schema_person.into() {
+        // count += 1;
+        Ok(()) as Result<(), TurtleError>
+    }).unwrap();
+  }
+  // println!("parsed {} statements", count);
 }
